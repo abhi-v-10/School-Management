3ac6fcfa1f44d4a4d7967091f037fd23d0550090
Abhishyanth <v.abhishyanth118@gmail.com>
2025-08-29 22:47:18 +0530

    Preparing for vercel deploy

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-# install python deps (safety)
-pip install -r requirements.txt
-
-# collect static into `staticfiles` directory
-python manage.py collectstatic --noinput --clear
+# Do NOT reinstall requirements here, Vercel already does that
+python3 manage.py collectstatic --noinput --clear
