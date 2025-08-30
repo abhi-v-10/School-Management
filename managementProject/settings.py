f51cc2159f1aac627ceaf732297492ef989574b1
Abhishyanth <v.abhishyanth118@gmail.com>
2025-08-30 12:11:18 +0530

    Preparing for vercel deploy

--- managementProject/settings.py
+++ managementProject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 AUTH_USER_MODEL = 'accountsApp.User'
-ALLOWED_HOSTS = ['127.0.0.1', 'school-management-ayag.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'school-management-ayag.vercel.app', 'scholaroid.vercel.app']
 
 
 # Application definition
